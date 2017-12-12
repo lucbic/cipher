edd095477da36eb9f3088689b6096b9e652f318e
Lucas Bicudo <lucbic@gmail.com>
2017-12-12 16:44:10 -0200

    Add shift parameters feature

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require_relative 'cipher'
 require 'sinatra'
+
 if development?
+  require 'pry'
   require 'sinatra/reloader'
 end
 
@@ -9,7 +11,7 @@ get '/' do
 end
 
 post "/" do
-  @ciphered = caesar_cipher(params[:to_cipher], 3)
+  @ciphered = caesar_cipher(params[:to_cipher], params[:shift].to_i, to_b(params[:direction]))
   erb :index
   # redirect "/"
 end
--- cipher.rb
+++ cipher.rb
@@ -1,18 +1,44 @@
+require 'pry'
+
+
 ALPHA = "abcdefghijklmnopfqrstuvwxyz"
 ALPHA_C = ALPHA.upcase
 
-def caesar_cipher(to_cipher, shift)
+def caesar_cipher(to_cipher, shift, direction)
   ciphered = String.new
 
   to_cipher.each_char do |i|
+  # binding.pry
     if ALPHA.index(i)
-      ciphered << ALPHA[ALPHA.index(i) - shift]
+      ciphered << increment(false, direction, i, shift)
     elsif ALPHA_C.index(i)
-      ciphered << ALPHA_C[ALPHA_C.index(i) - shift]
+      ciphered << increment(true, direction, i, shift)
     else
       ciphered << i
     end
   end
 
   ciphered
+end
+
+def increment(upcase, direction, char, shift)
+  if upcase && direction
+    ALPHA_C[ALPHA_C.index(char) + shift]
+  elsif upcase && !direction
+    ALPHA_C[ALPHA_C.index(char) - shift]
+  elsif !upcase && direction
+    ALPHA[ALPHA.index(char) + shift]
+  elsif !upcase && !direction
+    ALPHA[ALPHA.index(char) - shift]
+  else
+    nil
+  end
+end
+
+def to_b(value)
+  if value == 'true'
+    true
+  elsif value == 'false'
+    false
+  end
 end
